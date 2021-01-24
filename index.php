6643a1c4234a0590b92354e6ab4750adab037ff6
DevilDevloper <67404442+DevilDevloper@users.noreply.github.com>
2021-01-24 17:56:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 
+<html>
+<head>
+<title>Online PHP Script Execution</title>
+</head>
+<body>
 <?php
-
-echo "Welcome to do while loops <br>";
+  
+ echo "Welcome to do while loops <br>";
 $i = 0;
 do{
-    echo "$i <br>";
-    $i++;
+    echo "$i";
+    $i;
 }while($i<5);
-?>  
+ ?>
+</body>
+</html>
